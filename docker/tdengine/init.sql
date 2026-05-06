7fb58333b40026a832644eb3cb78b9e2dc06f1e8
bihaiqiongtian <bihaiqiongtian@noreply.gitcode.com>
2026-05-06 11:46:56 +0800

    优化一下文档；同时将-54.yml 和服务器环境同步

--- docker/tdengine/init.sql
+++ docker/tdengine/init.sql
@@ -5,5 +5,11 @@ CREATE DATABASE IF NOT EXISTS energy;
 -- 使用 energy 数据库
 USE energy;
 
+-- 创建 energy 用户（TDengine 3.x 语法）
+CREATE USER IF NOT EXISTS energy WITH PASSWORD 'difyai123456';
+
+-- 授权 energy 用户访问 energy 数据库
+GRANT ALL ON energy TO energy;
+
 -- 显示创建的数据库
 SHOW DATABASES;
